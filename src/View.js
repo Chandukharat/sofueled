5205b5a34d2f1c7071cbc448c2bb5dc92ce4da43
CHANDU VIJAY KHARAT <kharatc187@gmail.com>
2022-02-09 23:37:33 +0530

    added

---
+++ src/View.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import './Style.css'
+
+function View() {
+  return <div className='all' >
+      <div className='prof'>
+      <div className='Header'>
+          <img className='pic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMevJFZGY1xvPjPoTIoeQtYtBEr9W9ciCdSeYnh0fj9w_D8I6BWgCEYYlSccFJOucreuw&usqp=CAU' />
+          <div className='marg'>
+              <h5>GUEST-12345</h5>
+              <div><span><img className='log'  src='https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80'/></span>away</div>
+          </div>
+          <div><img className='vvv' src='https://static.thenounproject.com/png/204040-200.png'/></div>
+          <div><img className='vvv' src='https://library.kissclipart.com/20191023/qge/kissclipart-notifications-icon-0944b9061b6f136e.png'/></div>
+          <div><img className='vvv' src='https://cdn3.iconfinder.com/data/icons/basic-user-interface-5/64/dots_dot_dot-menu_option_nav_navigation_main-512.png'/></div>
+      </div>
+      <h3>CONTACTS</h3>
+      <div><input className='search' placeholder='search' /></div>
+      <div>A</div>
+      <div className='other'>
+          <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh_OL9r9XnYRdIN6U5_uS0k9OQjxsz1e1J3mryvm_Qm5gRXzo7REyF5txOArfY0NracD8&usqp=CAU'/>
+          <div className='marg'>
+              <h5>DAMN ADAMS</h5>
+              <div><span><img className='log' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEVl/wB6QsljAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII='/></span>i'm available</div>
+          </div>
+      </div>
+      <div className='other'>
+      <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4bNpPF6JEjN8xUULSxC7tWDnhceTOoRNggxIdtYOcRrj2ODBc86Pb88NbXiTx6vNWgsY&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Evylyn Allen</h5>
+              <div><span><img className='log' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEVl/wB6QsljAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII='/></span>i'm available</div>
+          </div>
+      </div>
+      <div>C</div>
+      <div className='other'>
+          <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXvTcs2fX2vDy5xOmSmE__4kHRsGgDhrzJWz_3HojsgS8mIgfWiRpznGxPkkKbxMGm8Qw&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Kun chang mins</h5>
+              <div><span><img className='log' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX+AAAYIGMAAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII='/></span>     i'm Busy</div>
+          </div>
+      </div>
+      <div className='other'>
+      <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl2txvlJ4C6-0UhnK5BKHh_4efyWjQl3rZtH7-FgElXeyeyuLR_eF8QGvMJib0L8NwbdE&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Heather clarkS</h5>
+              <div><span><img className='log' src='https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80'/></span>i'm away</div>
+          </div>
+      </div>
+      <div className='other'>
+      <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKbzlDFdy7FNr87M5cBqKpYmS4KBCabdzg28tVoqydsrHrCyUUwhXwrAlsru9zhPbO35I&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Barbara cortillaS</h5>
+              <div><span><img className='log' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEVl/wB6QsljAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII='/></span>i'm Available</div>
+          </div>
+      </div>
+      <div className='other'>
+      <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoI9zVavfjdtHg9qholQeqGAToghkg4v6sbmp52jZyiF0LDnTRZU1i_TkxI0HEs83CXvk&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Gypsie Cardienge</h5>
+              <div><span><img className='log' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEVl/wB6QsljAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII='/></span>i'm available</div>
+          </div>
+      </div>
+      <div>D</div>
+      <div className='other'>
+          <img className='picc' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeAuHQ6UKIsqOOLkwB0uNIuta1MmNMszVCZYx-vfYi4d9YrS0ta_A1wi0Dp1UL1E-jb8k&usqp=CAU'/>
+          <div className='marg'>
+              <h5>Gauthier DRavitt</h5>
+              <div><span><img className='log' src='https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80'/></span>i'm away</div>
+          </div>
+      </div>
+      <div className='footer'>
+          <div><img className='vvv' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6aknXY0P1svZ8yaWve8c0TqRySQeTHDNlsBEyaq0c7GAHr1_6yNccBKi-WzlpovOUoTs&usqp=CAU'/></div>
+          <div><img className='vvv' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLJxB2CCNrsLm7wIKjhgvcCV91yW6MSqt1djKqEzXGJOvGPD7Y4wsv7X7BbANOM1Oa0ks&usqp=CAU'/></div>
+          <div><img className='vvv' src='https://www.kindpng.com/picc/m/180-1801005_text-message-icon-png-text-bubble-icon-grey.png'/></div>
+          <div><img className='vvv' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuUs-lNGkNGRiqedCuBAIIeoB7y4eMpPeKDm2wUSGof3JaPFgBYT7NTxR9jDWWyZnNhvw&usqp=CAU'/></div>
+      </div>
+</div>
+<div className='second' >
+   <div className='sec'>Select a chart to start messaging</div> 
+</div>
+  </div>;
+}
+
+export default View;
