f58ac20ca230bbe1d405cf70348a957d532a6c75
CHANDU VIJAY KHARAT <kharatc187@gmail.com>
2022-02-13 12:04:46 +0530

    done

--- src/View.js
+++ src/View.js
@@ -2,42 +2,50 @@ import React from "react";
 import "./Style.css";
 
 function View() {
+  const [showResults, setShowResults] = React.useState([])
+  const onChan = (e) => setShowResults(e.target.value)
   return (
     <div className="all">
       <div className="prof">
         <div className="Header">
           <div className="content">
-           
             <div className="marg">
-            <img
-              className="pic"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMevJFZGY1xvPjPoTIoeQtYtBEr9W9ciCdSeYnh0fj9w_D8I6BWgCEYYlSccFJOucreuw&usqp=CAU"
-            />
-              <h5 className="HeadName">GUEST-12345 <div className="Headcont">
-                <span>
-                  <img
-                    className="log"
-                    src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                  />
-                </span>
-                <span className="statuss" > Away </span>
-              </div></h5>
-             
+              <img
+                className="pic"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMevJFZGY1xvPjPoTIoeQtYtBEr9W9ciCdSeYnh0fj9w_D8I6BWgCEYYlSccFJOucreuw&usqp=CAU"
+              />
+              <h5 className="HeadName">
+                GUEST-12345{" "}
+                <div className="Headcont">
+                  <span>
+                    <img
+                      className="log"
+                      src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                    />
+                  </span>
+                  <span className="statuss"> Away </span>
+                </div>
+              </h5>
             </div>
             <div>
               <img className="note" src="icons8-note-32.png" />
-           
-            
+
               <img className="vvv" src="icons8-notification-32.png" />
-            
+
               <img className="vvv" src="icons8-menu-vertical-30.png" />
             </div>
           </div>
         </div>
         <h3 className="cont">Contacts</h3>
+<div className="searchicon">
+        
+        { showResults.length===0 ? <div id="button-holder">
+          <img id="button-holder img" src="https://p7.hiclipart.com/preview/583/846/459/magnifying-glass-loupe-icon-magnifying-glass-icon.jpg" />
+        </div> : null }
 
         <div>
-          <input className="search" placeholder="                Search" />
+          <input className="search" placeholder="           Search" onChange={onChan} />
+        </div>
         </div>
 
         <div className="midd">
@@ -48,16 +56,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh_OL9r9XnYRdIN6U5_uS0k9OQjxsz1e1J3mryvm_Qm5gRXzo7REyF5txOArfY0NracD8&usqp=CAU"
             />
             <div className="marg">
-              <h5 >Darren Adamns <div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
-                  /> <span className="status" >i'm Available </span>
-                </span>
-             
-              </div></h5>
-             
+              <h5>
+                Darren Adamns{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
+                    />{" "}
+                    <span className="status">i'm Available </span>
+                  </span>
+                </div>
+              </h5>
             </div>
           </div>
           <div className="other">
@@ -66,17 +76,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4bNpPF6JEjN8xUULSxC7tWDnhceTOoRNggxIdtYOcRrj2ODBc86Pb88NbXiTx6vNWgsY&usqp=CAU"
             />
             <div className="marg">
-              <h5>Evylyn Allen <div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
-                  /><span className="status" >i'm Available </span>
-                </span>
-                
-              </div>
+              <h5>
+                Evylyn Allen{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
+                    />
+                    <span className="status">i'm Available </span>
+                  </span>
+                </div>
               </h5>
-             
             </div>
           </div>
           <div className="def">C</div>
@@ -86,16 +97,18 @@ function View() {
               src="https://images.unsplash.com/photo-1525457136159-8878648a7ad0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80"
             />
             <div className="marg">
-              <h5>Kun chang mins <div>
-                <span>
-                  <img
-                    className="log"
-                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX+AAAYIGMAAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="
-                  />
-                </span>{" "}
-                <span className="status" >i'm Busy </span>
-              </div></h5>
-             
+              <h5>
+                Kun chang mins{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX+AAAYIGMAAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="
+                    />
+                  </span>{" "}
+                  <span className="status">i'm Busy </span>
+                </div>
+              </h5>
             </div>
           </div>
           <div className="other">
@@ -104,16 +117,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2tjcIPkT1GZZAPwNPzndMoVhxxhqI1vOJ3g&usqp=CAU"
             />
             <div className="marg">
-              <h5>Heather Clarks <div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                  />
-                </span>
-                <span className="status" >i'm Away </span>
-              </div></h5>
-             
+              <h5>
+                Heather Clarks{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                    />
+                  </span>
+                  <span className="status">i'm Away </span>
+                </div>
+              </h5>
             </div>
           </div>
           <div className="other">
@@ -122,16 +137,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKbzlDFdy7FNr87M5cBqKpYmS4KBCabdzg28tVoqydsrHrCyUUwhXwrAlsru9zhPbO35I&usqp=CAU"
             />
             <div className="marg">
-              <h5>Barbara Cortillas <div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
-                  />
-                </span>
-                <span className="status" >i'm Available </span>
-              </div></h5>
-             
+              <h5>
+                Barbara Cortillas{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
+                    />
+                  </span>
+                  <span className="status">i'm Available </span>
+                </div>
+              </h5>
             </div>
           </div>
           <div className="other">
@@ -140,16 +157,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoI9zVavfjdtHg9qholQeqGAToghkg4v6sbmp52jZyiF0LDnTRZU1i_TkxI0HEs83CXvk&usqp=CAU"
             />
             <div className="marg">
-              <h5>Gypsie Cardienge<div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
-                  />
-                </span>
-                <span className="status" >i'm Available </span>
-              </div></h5>
-              
+              <h5>
+                Gypsie Cardienge
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png"
+                    />
+                  </span>
+                  <span className="status">i'm Available </span>
+                </div>
+              </h5>
             </div>
           </div>
           <div className="def">D</div>
@@ -159,16 +178,18 @@ function View() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeAuHQ6UKIsqOOLkwB0uNIuta1MmNMszVCZYx-vfYi4d9YrS0ta_A1wi0Dp1UL1E-jb8k&usqp=CAU"
             />
             <div className="marg">
-              <h5>Gauthier Dravitt  <div>
-                <span>
-                  <img
-                    className="log"
-                    src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                  />
-                </span>
-                <span className="status" >i'm Away </span>
-              </div></h5>
-             
+              <h5>
+                Gauthier Dravitt{" "}
+                <div>
+                  <span>
+                    <img
+                      className="log"
+                      src="https://images.unsplash.com/flagged/photo-1593005510329-8a4035a7238f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8eWVsbG93fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                    />
+                  </span>
+                  <span className="status">i'm Away </span>
+                </div>
+              </h5>
             </div>
           </div>
         </div>
